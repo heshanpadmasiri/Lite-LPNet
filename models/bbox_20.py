62bf03717f49bbb67f2945c9ec61829920cb912a
Heshan <hpheshan@gmail.com>
2020-08-29 04:11:22 +0530

    added v20 bbox

---
+++ models/bbox_20.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models, Input
+from models.nn_blocks import mobilenet_block_1, mobilenet_block_2
+
+def __get_model__(feature_extractor,extractor_layer=None):
+    global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
+    prediction_layer = tf.keras.layers.Dense(4)
+    model = tf.keras.Sequential([
+      feature_extractor,
+      global_average_layer,
+      prediction_layer
+    ])
+
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    return model
+
+
+def __get_backborn__(input_size):
+    image = Input(shape=input_size,name='img')
+    x = mobilenet_block_1(image)
+    x = mobilenet_block_2(x)
+    x = mobilenet_block_1(x)
+    x = mobilenet_block_2(x)
+    x = mobilenet_block_1(x)
+    x = mobilenet_block_1(x)
+    x = mobilenet_block_2(x)
+    x = mobilenet_block_1(x)
+    x = mobilenet_block_1(x)
+    x = mobilenet_block_2(x)
+    x = mobilenet_block_1(x)
+    x = mobilenet_block_1(x)
+    x = mobilenet_block_2(x)
+    backborn = tf.keras.Model(inputs=image, outputs=x)
+    return backborn
+
+def create_model(input_shape):
+    backborn = __get_backborn__(input_shape)
+    model = __get_model__(backborn)
+    return model
--- models/bbox_dispatcher.py
+++ models/bbox_dispatcher.py
@@ -1,7 +1,9 @@
+from models.bbox_20 import create_model as create_model_20
 from models.bbox_19 import create_model as create_model_19
 
 CONSTRUCTORS = {
-    'v19' : create_model_19
+    'v19' : create_model_19,
+    'v20' : create_model_20
 }
 
 def create_model(model_name,input_shape):
