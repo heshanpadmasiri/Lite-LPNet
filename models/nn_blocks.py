e89b4aa51568a14616d30938926432b93b9d846c
Heshan <hpheshan@gmail.com>
2020-09-03 22:58:24 +0530

    bugfix: relu activation is now used in rpnet blocks

--- models/nn_blocks.py
+++ models/nn_blocks.py
@@ -62,8 +62,8 @@ def mobilenet_block_1(inputs):
     return layers.Add()([inputs, x])
 
 def rpnet_block_1(inputs,k=5):
-    X = layers.Conv2D(64, (1, k), padding='same')(inputs)
-    X = layers.Conv2D(64, (k, 1), padding='same', strides=2)(X)
+    X = layers.Conv2D(64, (1, k), padding='same', activation='relu')(inputs)
+    X = layers.Conv2D(64, (k, 1), padding='same', strides=2, activation='relu')(X)
     X = layers.MaxPool2D((2,2), strides=2)(X)
     return X
 
@@ -76,13 +76,13 @@ def rpnet_block_1_seq(k=5):
     
 def rpnet_block_2(filters=[64,64]):
     block = models.Sequential()
-    block.add(layers.Conv2D(filters[0], (5,5), strides=2,padding='same'))
+    block.add(layers.Conv2D(filters[0], (5,5), strides=2,padding='same', activation='relu'))
     block.add(layers.MaxPool2D((2,2), strides=2))
-    block.add(layers.Conv2D(filters[1], (5,5), strides=2, padding='same'))
+    block.add(layers.Conv2D(filters[1], (5,5), strides=2, padding='same', activation='relu'))
     block.add(layers.MaxPool2D((2,2), strides=2))
     return block
 
 def rpnet_block_3(inputs,k=5):
-    X = layers.Conv2D(64, (1, k), padding='same')(inputs)
-    X = layers.Conv2D(64, (k, 1), padding='same', strides=2)(X)
+    X = layers.Conv2D(64, (1, k), padding='same', activation='relu')(inputs)
+    X = layers.Conv2D(64, (k, 1), padding='same', strides=2, activation='relu')(X)
     return X
