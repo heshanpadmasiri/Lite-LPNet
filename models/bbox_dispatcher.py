b0f66535ef5a370b4a47dcb9b0e23993692b4907
Heshan <hpheshan@gmail.com>
2020-09-16 10:59:17 +0530

    bbox v26 added

--- models/bbox_dispatcher.py
+++ models/bbox_dispatcher.py
@@ -8,20 +8,23 @@ from models.bbox_22 import create_model as create_model_22
 from models.bbox_23 import create_model as create_model_23
 from models.bbox_24 import create_model as create_model_24
 from models.bbox_25 import create_model as create_model_25
+from models.bbox_26 import create_model as create_model_26
 
 CONSTRUCTORS = {
-    'v16' : create_model_16,
-    'v17' : create_model_17,
-    'v18' : create_model_18,
-    'v19' : create_model_19,
-    'v20' : create_model_20,
-    'v21' : create_model_21,
-    'v22' : create_model_22,
-    'v23' : create_model_23,
-    'v24' : create_model_24,
-    'v25' : create_model_25
+    'v16': create_model_16,
+    'v17': create_model_17,
+    'v18': create_model_18,
+    'v19': create_model_19,
+    'v20': create_model_20,
+    'v21': create_model_21,
+    'v22': create_model_22,
+    'v23': create_model_23,
+    'v24': create_model_24,
+    'v25': create_model_25,
+    'v26': create_model_26
 }
 
-def create_model(model_name,input_shape):
+
+def create_model(model_name, input_shape):
     model_name = model_name.lower()
     return CONSTRUCTORS[model_name](input_shape)
