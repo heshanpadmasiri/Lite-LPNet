8341b5fd539850b95c296772da793b89bd2250b4
Heshan <hpheshan@gmail.com>
2020-11-01 16:56:37 +0530

    added lp timing script

--- latency.py
+++ latency.py
@@ -1,25 +1,42 @@
-from models.bbox_dispatcher import create_model, CONSTRUCTORS
+from models import bbox_dispatcher as bbox
+from models import lp_dispatcher as lp
 import time
 import tensorflow as tf
 import joblib
 
-TARGET_SIZE = (480, 480)
 BATCH_SIZE = 30
 
 
-def measure_stage_1_latency():
-    models = list(CONSTRUCTORS.keys())
+def measure_stage_2_latency():
+    TARGET_SIZE = (280, 560)
+    models = list(lp.CONSTRUCTORS.keys())
     timings = {}
     input_tensor = tf.random.uniform((BATCH_SIZE, ) + TARGET_SIZE + (3, ))
     for model_name in models:
-        model = create_model(model_name, TARGET_SIZE + (3, ))
+        model = lp.create_model(model_name, TARGET_SIZE + (3, ))
         start = time.process_time_ns()
         model(input_tensor)
         end = time.process_time_ns()
-        timings[model_name] = end - start
+        timings[model_name] = (end - start) / BATCH_SIZE
+    print(timings)
+    joblib.dump(timings, 'lp_timing.pkl')
+
+
+def measure_stage_1_latency():
+    TARGET_SIZE = (480, 480)
+    models = list(bbox.CONSTRUCTORS.keys())
+    timings = {}
+    input_tensor = tf.random.uniform((BATCH_SIZE, ) + TARGET_SIZE + (3, ))
+    for model_name in models:
+        model = bbox.create_model(model_name, TARGET_SIZE + (3, ))
+        start = time.process_time_ns()
+        model(input_tensor)
+        end = time.process_time_ns()
+        timings[model_name] = (end - start) / BATCH_SIZE
     print(timings)
     joblib.dump(timings, 'bbox_timing.pkl')
 
 
 if __name__ == '__main__':
     measure_stage_1_latency()
+    measure_stage_2_latency()
